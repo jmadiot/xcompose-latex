d3fc5e0da5779b1335865b12934f2735252886fd
Jean-Marie Madiot <madiot@gmail.com>
2014-01-30 16:43:50 +0100

    perl script, already sufficient

---
+++ xcompose-latex.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+
+while(<>){
+  if($_ =~ m/\\([A-Za-z]+).*([^\x00-\x7F])/) {
+    $char = $2;
+    my $code = $1;
+    (my $xcode = $1) =~ s/(.)/<$1> /g;
+    print "<Multi_key> <backslash> $xcode: \"$char\"  # latex \\$code\n";
+  } else {
+    if($_ =~ m/^[A-Za-z0-9 ]*/) {
+      # do nothing, only boring characters here
+    } else {
+      # maybe something missed here
+      print "# unrecognized line: $_"
+    }
+  }
+}
+
+
+# one-liner:
+# perl -e 'binmode(STDIN, ":utf8");binmode(STDOUT, ":utf8");while(<>){if($_ =~ m/\\([A-Za-z]+).*([^\x00-\x7F])/){$char=$2;my $code = $1;(my $xcode = $1) =~ s/(.)/<$1> /g;print "<Multi_key> <backslash> $xcode: \"$char\"  # latex \\$code\n";}}'
